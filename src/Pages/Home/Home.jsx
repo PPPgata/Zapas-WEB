28221b67822805234d11f41bb00e36fa0126b3f5
rafael-leivas <rafaelblsteam@gmail.com>
2024-05-18 11:36:03 -0300

    .

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import NavBar from '../../Components/NavBar/NavBar'
-import st from "./Home.module.css"
-import ButtonLogin from '../../Components/Button Login/ButtonLogin'
+import React from "react";
+import NavBar from "../../Components/NavBar/NavBar";
+import st from "./Home.module.css";
 
 const Home = () => {
   return (
@@ -11,19 +10,46 @@ const Home = () => {
           <NavBar />
         </header>
         <main>
+          <div className={st.container_1}>
+            <div className={st.textsContainer_1}>
+              <h2>SISTEMA DE ESTOQUE</h2>
+              <h2 className={st.textZapas}>ZAPAS</h2>
+              <h1>Controle seu estoque com facilidade!</h1>
+              <span>
+                Otimize seu estoque com nossa Implantação de Sistema de Estoque.
+                Acesso remoto e alertas de baixo estoque inclusos!
+              </span>
+              <a className={st.buttonCadastro} href="#">
+                Cadastre - se
+              </a>
+            </div>
 
-          <div>
-            <h2>SISTEMA DE ESTOQUE</h2>
-            <h2>ZAPAS</h2>
-            <h1>Controle seu estoque com facilidade!</h1>
-            <span>Otimize seu estoque com nossa Implantação de Sistema de Estoque. Acesso remoto e alertas de baixo estoque inclusos!</span>
-            <ButtonLogin name={ "Cadastre-se" } link={"#"}/>
+            <div className={st.formsContainer}>
+              <div className={st.itemsForms}>
+                <h2>Potencialize seu estoque com a Zapas!</h2>
+                <p>
+                  Transforme sua gestão de estoque com eficiência. Solicite um
+                  orçamento personalizado e descubra como otimizar seus
+                  processos agora.
+                </p>
+                <ul>
+                  <li>
+                    <input placeholder="Name ..." type="text" />
+                  </li>
+                  <li>
+                    <input placeholder="E-mail ..." type="text" />
+                  </li>
+                  <li>
+                    <input placeholder="Mensagem ..." type="text" />
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-
         </main>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
