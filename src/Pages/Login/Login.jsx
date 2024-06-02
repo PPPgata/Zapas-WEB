d0136ba66ddd95e6ab41d85537c432aace1bc14e
rafael-leivas <rafaelblsteam@gmail.com>
2024-06-02 18:55:52 -0300

    Iniciando estilização da tela de login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import st from "./Login.module.css";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as yup from "yup";
+import teamWork from "../../assets/img/Team_work.png";
 
 const Login = () => {
   const handleClickLogin = (values) => console.log(values);
@@ -19,40 +20,50 @@ const Login = () => {
 
   return (
     <>
-      <div className={st.container}>
-        <h1>Sign Up</h1>
-        <Formik
-          initialValues={{ email: "", password: "" }}
-          onSubmit={handleClickLogin}
-          validationSchema={validationLogin}
-        >
-          <Form className="login-form">
-            <div className="login-group">
-              <Field name="email" className="form-field" placeholder="Email" />
-              <ErrorMessage
-                name="email"
-                component="span"
-                className="form-error"
-              />
-            </div>
-            <div className="login-group">
-              <Field
-                name="password"
-                className="form-field"
-                placeholder="Senha"
-                type="password"
-              />
-              <ErrorMessage
-                name="password"
-                component="span"
-                className="form-error"
-              />
-            </div>
-            <button className="button" type="submit">
-              Login
-            </button>
-          </Form>
-        </Formik>
+      <div className={st.content}>
+        <div className={st.welcome}>
+          <h1>Bem vindo!</h1>
+          <img src={teamWork} alt="Pessoas carregando blocos de brinquedo" />
+        </div>
+        <div className={st.form}>
+          <h1>Sign Up</h1>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            onSubmit={handleClickLogin}
+            validationSchema={validationLogin}
+          >
+            <Form className="login-form">
+              <div className="login-group">
+                <Field
+                  name="email"
+                  className={st.form_field}
+                  placeholder="Email"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="span"
+                  className={st.form_error}
+                />
+              </div>
+              <div className="login-group">
+                <Field
+                  name="password"
+                  className={st.form_field}
+                  placeholder="Senha"
+                  type="password"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="span"
+                  className={st.form_error}
+                />
+              </div>
+              <button className="button" type="submit">
+                Login
+              </button>
+            </Form>
+          </Formik>
+        </div>
       </div>
     </>
   );
