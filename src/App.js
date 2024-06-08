c54dfc9735baefd516f2cc1ee0ac207005f27e71
rafael-leivas <rafaelblsteam@gmail.com>
2024-06-08 18:42:54 -0300

    add title in page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+
+export const metadata = {
+  title: "Zapas",
+  description: "Zapas é uma plataforma de doações de roupas",
+  image: "https://zapas.vercel.app/assets/img/LogoZ.svg",
+};
 
 function App() {
+
+  useEffect(() => {
+    document.title = metadata.title;
+  }, []);
   return (
+    
     <div className="App">
       <Outlet />
     </div>
