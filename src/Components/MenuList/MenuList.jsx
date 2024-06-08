4b61364acfe5a795004c21ce4ee4bd1cfe418f08
rafael-leivas <rafaelblsteam@gmail.com>
2024-06-08 17:41:38 -0300

    make a SideBar

--- src/Components/MenuList/MenuList.jsx
+++ src/Components/MenuList/MenuList.jsx
@@ -1,22 +1,56 @@
+import st from "./MenuList.module.css";
 import { Menu } from "antd";
-import { HomeOutlined, InboxOutlined, PlusOutlined, UsergroupAddOutlined, LogoutOutlined } from "@ant-design/icons";
- 
+import {
+  HomeOutlined,
+  InboxOutlined,
+  PlusOutlined,
+  UsergroupAddOutlined,
+  LogoutOutlined,
+  HistoryOutlined,
+} from "@ant-design/icons";
+
 const MenuList = () => {
   return (
-    <Menu theme="dark">
-      <Menu.Item key="home" icon={<HomeOutlined />}>
+    <Menu
+      theme="dark"
+      mode="inline"
+      className={st.menu_bar}
+      defaultSelectedKeys={["home"]}
+    >
+      <Menu.Item
+        key="home"
+        icon={<HomeOutlined style={{ fontSize: "23px" }} />}
+      >
         Home
       </Menu.Item>
-      <Menu.Item key="home" icon={<InboxOutlined />}>
+      <Menu.Item
+        key="stock"
+        icon={<InboxOutlined style={{ fontSize: "23px" }} />}
+      >
         Estoques
       </Menu.Item>
-      <Menu.Item key="home" icon={<PlusOutlined />}>
-        Items
+      <Menu.Item
+        key="itens"
+        icon={<PlusOutlined style={{ fontSize: "23px" }} />}
+      >
+        Itens
       </Menu.Item>
-      <Menu.Item key="home" icon={<UsergroupAddOutlined />}>
+      <Menu.Item
+        key="users"
+        icon={<UsergroupAddOutlined style={{ fontSize: "23px" }} />}
+      >
         Usuários
       </Menu.Item>
-      <Menu.Item key="home" icon={<LogoutOutlined />}>
+      <Menu.Item
+        key="history"
+        icon={<HistoryOutlined style={{ fontSize: "23px" }} />}
+      >
+        Histórico
+      </Menu.Item>
+      <Menu.Item
+        key="logOut"
+        icon={<LogoutOutlined style={{ fontSize: "23px" }} />}
+      >
         Sair
       </Menu.Item>
     </Menu>
--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
 import st from "../SideBar/SideBar.module.css";
 import { Layout } from "antd";
-import Logo from '../Logo/Logo';
-import MenuList from '../MenuList/MenuList';
+import Logo from "../Logo/Logo";
+import MenuList from "../MenuList/MenuList";
+import SmallLogo from '../SmallLogo/SmallLogo';
 
-const {Header, Sider} = Layout;
+const { Header, Sider } = Layout;
 
 const SideBar = () => {
+  const [collapsed, setCollapsed] = useState(true);
+
   return (
     <>
       <Layout>
-        <Sider className={st.sidebar} style={{ backgroundColor: '#0081A7' }}>
-          <Logo />
+        <Sider
+          className={st.sidebar}
+          width={250}
+          collapsedWidth={80}
+          collapsed={collapsed}
+          onMouseEnter={() => setCollapsed(false)}
+          onMouseLeave={() => setCollapsed(true)}
+          style={{ backgroundColor: "#002249", transition: 'all 0.3s' }}
+        >
+          {collapsed ? <SmallLogo /> : <Logo className={st.logo} />}
           <MenuList />
+          {/* Adicionar o elemento da faixa */}
+          {!collapsed && <div className={st.rightStripe}></div>}
         </Sider>
       </Layout>
     </>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
