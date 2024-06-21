a411c6db955b4533149653eff257d29b954bd1bf
rafael-leivas <rafaelblsteam@gmail.com>
2024-06-21 12:21:31 -0300

    Criando connexão com banco de dados

--- server/index.js
+++ server/index.js
@@ -1,17 +1,67 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const cors = require("cors");
 
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "root",
-  database: "zapas",
+  database: "zapasbd",
 });
 
-app.get("/", (req, res) => {
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const senha = req.body.senha;
+  const nome = req.body.nome;
+  const segmento = req.body.segmento;
+  const cnpj = req.body.cnpj;
+
+  db.query("SELECT * FROM empresas WHERE email = ?", [email], (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    if (result.length == 0) {
+      db.query(
+        "INSERT INTO empresas (email, senha, nome, segmento, cnpj) VALUES (?, ?, ?, ?, ?)",
+        [email, senha, nome, segmento, cnpj],
+        (err, response) => {
+          if (err) {
+            res.send(err);
+          }
+
+          res.send({ msg: "Cadastro realizado com sucesso!" });
+        }
+      );
+    } else {
+      res.send({ msg: "Email já cadastrado!" });
+    }
+  });
+});
+
+app.post("/login", (req, res) => {
+  const { email, senha } = req.body;
+
   db.query(
-    "INSERT INTO empresas (email, password, enterprise_name, industry, cnpj) VALUES ('rafaelleivas@gmail.com', '123456', 'Zapas', 'Tecnologia', '12312312312312')"
+    "SELECT * FROM empresas WHERE email = ? AND senha = ?",
+    [email, senha],
+    
+    (err, result) => {
+      if (err) {
+        console.error("Erro na consulta SQL:", err);
+        res.status(500).send(err); // Erro interno do servidor
+        return;
+      }
+
+      if (result.length > 0) {
+        res.send({ msg: "Login realizado com sucesso!" });
+      } else {
+        res.send({ msg: "Email ou senha incorretos!" });
+      }
+    }
   );
 });
 
