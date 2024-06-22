07c0090199f1ec0313a85c3294e92f889a6ebe82
rafael-leivas <rafaelblsteam@gmail.com>
2024-06-22 01:39:41 -0300

    valitating input of cnpj

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import st from "./Login.module.css";
 import { Formik, Form, Field, ErrorMessage } from "formik";
@@ -9,7 +8,6 @@ import { Link } from "react-router-dom";
 import Axios from "axios";
 
 const Login = () => {
-
   const [loginError, setLoginError] = useState('');
   const navigate = useNavigate();
 
@@ -17,15 +15,20 @@ const Login = () => {
     Axios.post("http://localhost:3001/login", {
       email: values.email,
       senha: values.password,
-    }).then((response) => {
+    })
+    .then((response) => {
       console.log(response);
       if (response.data.msg === 'Login realizado com sucesso!') {
         localStorage.setItem('token', response.data.token);
         navigate('/dashboard'); 
-      }else {
+      } else {
         setLoginError(response.data.msg);
       }
     })
+    .catch((error) => {
+      console.error('Erro na requisição:', error);
+      setLoginError('Erro ao realizar login. Tente novamente mais tarde.');
+    });
   };
 
   const validationLogin = yup.object().shape({
@@ -40,24 +43,24 @@ const Login = () => {
   });
 
   return (
-    <>
-      <div className={st.content}>
-        <div className={st.welcome}>
-          <h1>Bem vindo!</h1>
-          <img src={teamWork} alt="Pessoas carregando blocos de brinquedo" />
-        </div>
-        <div className={st.form}>
-          <h1>Realize seu login</h1>
-          <Formik
-            initialValues={{ email: "", password: "" }}
-            onSubmit={handleClickLogin}
-            validationSchema={validationLogin}
-          >
+    <div className={st.content}>
+      <div className={st.welcome}>
+        <h1>Bem vindo!</h1>
+        <img src={teamWork} alt="Pessoas carregando blocos de brinquedo" />
+      </div>
+      <div className={st.form}>
+        <h1>Realize seu login</h1>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={handleClickLogin}
+          validationSchema={validationLogin}
+        >
+          {({ errors, touched }) => (
             <Form className={st.login_form}>
               <div className="login-group">
                 <Field
                   name="email"
-                  className={st.form_field}
+                  className={`${st.form_field} ${errors.email && touched.email ? st.form_field_error : ''}`}
                   placeholder="Email"
                 />
                 <ErrorMessage
@@ -69,7 +72,7 @@ const Login = () => {
               <div className="login-group">
                 <Field
                   name="password"
-                  className={st.form_field}
+                  className={`${st.form_field} ${errors.password && touched.password ? st.form_field_error : ''}`}
                   placeholder="Senha"
                   type="password"
                 />
@@ -79,11 +82,12 @@ const Login = () => {
                   className={st.form_error}
                 />
               </div>
+              {loginError && <div className={st.error_message}>{loginError}</div>}
               <div className={st.links}>
                 <p>
                   Não tem cadastro?{" "}
                   <Link className={st.link} to={"/cadastro"}>
-                    Cadastre - se
+                    Cadastre-se
                   </Link>
                 </p>
                 <p>
@@ -93,15 +97,13 @@ const Login = () => {
                 </p>
               </div>
               <button className={st.button} type="submit">
-                {/* <Link className={st.linkButton}> */}
-                  Login
-                {/* </Link> */}
+                Login
               </button>
             </Form>
-          </Formik>
-        </div>
+          )}
+        </Formik>
       </div>
-    </>
+    </div>
   );
 };
 
