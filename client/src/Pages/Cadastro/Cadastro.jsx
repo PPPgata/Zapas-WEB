657517a3b77d0a4e5a23c44e2c203ff1a4ce22a1
rafael-leivas <rafaelblsteam@gmail.com>
2024-06-22 01:39:56 -0300

    valitating input of cnpj

--- client/src/Pages/Cadastro/Cadastro.jsx
+++ client/src/Pages/Cadastro/Cadastro.jsx
@@ -1,33 +1,33 @@
 import React from "react";
 import { useState } from "react";
 import st from "../Cadastro/Cadastro.module.css";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field } from "formik";
 import * as yup from "yup";
 import { Link, useNavigate } from "react-router-dom";
 import teamWork from "../../assets/img/Team_work.png";
 import Axios from "axios";
+import InputMask from "react-input-mask";
 
 const Cadastro = () => {
-
   const [CadastroError, setCadastroError] = useState('');
+  const [formSubmitted, setFormSubmitted] = useState(false);
   const navigate = useNavigate();
-  
+
   const handleClickRegister = (values) => {
     Axios.post("http://localhost:3001/register", {
       email: values.email,
       senha: values.password,
       nome: values.enterpriseName,
       segmento: values.enterpriseSegment,
-      cnpj: values.cnpj,
+      cnpj: values.cnpj.replace(/[^\d]/g, ''),
     }).then((response) => {
       console.log(response);
       if (response.data.msg === 'Cadastro realizado com sucesso!') {
         navigate('/login'); 
-      }else {
-        setCadastroError(response.data.msg); // Mostrar mensagem de erro
+      } else {
+        setCadastroError(response.data.msg);
       }
-
-    })
+    });
   };
 
   const validationRegister = yup.object().shape({
@@ -47,8 +47,8 @@ const Cadastro = () => {
     enterpriseSegment: yup.string().required("Este campo é obrigatório"),
     cnpj: yup
       .string()
-      .min(14, "O CNPJ deve ter 14 caracteres")
-      .max(14, "O CNPJ deve ter 14 caracteres")
+      .min(18, "O CNPJ deve ter 14 caracteres")
+      .max(18, "O CNPJ deve ter 14 caracteres")
       .required("Este campo é obrigatório"),
   });
 
@@ -68,96 +68,94 @@ const Cadastro = () => {
           onSubmit={handleClickRegister}
           validationSchema={validationRegister}
         >
-          <Form className={st.signUp_form}>
-            <div className={st.register_form}>
-              <Field
-                name="email"
-                className={st.form_field}
-                placeholder="Email"
-              />
-              <ErrorMessage
-                name="email"
-                component="span"
-                className={st.form_error}
-              />
-            </div>
-            <div className={st.passwords}>
+          {({ errors, touched }) => (
+            <Form className={st.signUp_form}>
               <div className={st.register_form}>
                 <Field
-                  name="password"
-                  className={st.form_field}
-                  placeholder="Senha"
-                  type="password"
+                  name="email"
+                  className={`${st.form_field} ${touched.email && errors.email ? st.error : ""}`}
+                  placeholder="Email"
                 />
-                <ErrorMessage
-                  name="password"
-                  component="span"
-                  className={st.form_error}
-                />
+                {touched.email && errors.email && (
+                  <span className={st.form_error}>{errors.email}</span>
+                )}
+              </div>
+              <div className={st.passwords}>
+                <div className={st.register_form}>
+                  <Field
+                    name="password"
+                    className={`${st.form_field} ${touched.password && errors.password ? st.error : ""}`}
+                    placeholder="Senha"
+                    type="password"
+                  />
+                  {touched.password && errors.password && (
+                    <span className={st.form_error}>{errors.password}</span>
+                  )}
+                </div>
+                <div className={st.register_form}>
+                  <Field
+                    name="confirmPassword"
+                    className={`${st.form_field} ${touched.confirmPassword && errors.confirmPassword ? st.error : ""}`}
+                    placeholder="Confirme sua senha"
+                    type="password"
+                  />
+                  {touched.confirmPassword && errors.confirmPassword && (
+                    <span className={st.form_error}>{errors.confirmPassword}</span>
+                  )}
+                </div>
               </div>
               <div className={st.register_form}>
                 <Field
-                  name="confirmPassword"
-                  className={st.form_field}
-                  placeholder="Confirme sua senha"
-                  type="password"
+                  name="enterpriseName"
+                  className={`${st.form_field} ${touched.enterpriseName && errors.enterpriseName ? st.error : ""}`}
+                  placeholder="Nome da sua empresa"
                 />
-                <ErrorMessage
-                  name="confirmPassword"
-                  component="span"
-                  className={st.form_error}
-                />
+                {touched.enterpriseName && errors.enterpriseName && (
+                  <span className={st.form_error}>{errors.enterpriseName}</span>
+                )}
               </div>
-            </div>
-            <div className={st.register_form}>
-              <Field
-                name="enterpriseName"
-                className={st.form_field}
-                placeholder="Nome da sua empresa"
-              />
-              <ErrorMessage
-                name="enterpriseName"
-                component="span"
-                className={st.form_error}
-              />
-            </div>
-            <div className={st.enterprise}>
-              <div className={st.register_form}>
-                <Field
-                  name="enterpriseSegment"
-                  className={st.form_field}
-                  placeholder="Segmento"
-                />
-                <ErrorMessage
-                  name="enterpriseSegment"
-                  component="span"
-                  className={st.form_error}
-                />
+              <div className={st.enterprise}>
+                <div className={st.register_form}>
+                  <Field
+                    name="enterpriseSegment"
+                    className={`${st.form_field} ${touched.enterpriseSegment && errors.enterpriseSegment ? st.error : ""}`}
+                    placeholder="Segmento"
+                  />
+                  {touched.enterpriseSegment && errors.enterpriseSegment && (
+                    <span className={st.form_error}>{errors.enterpriseSegment}</span>
+                  )}
+                </div>
+                <div className={st.register_form}>
+                  <Field name="cnpj">
+                    {({ field }) => (
+                      <InputMask
+                        {...field}
+                        mask="99.999.999/9999-99"
+                        className={`${st.form_field} ${touched.cnpj && errors.cnpj ? st.error : ""}`}
+                        placeholder="CNPJ"
+                      />
+                    )}
+                  </Field>
+                  {touched.cnpj && errors.cnpj && (
+                    <span className={st.form_error}>{errors.cnpj}</span>
+                  )}
+                </div>
               </div>
-              <div className={st.register_form}>
-                <Field
-                  name="cnpj"
-                  className={st.form_field}
-                  placeholder="CNPJ"
-                  type="number"
-                />
-                <ErrorMessage
-                  name="cnpj"
-                  component="span"
-                  className={st.form_error}
-                />
-              </div>
-            </div>
-            <p className={st.register}>
-              Já tem cadastro?{" "}
-              <Link className={st.link} to="/login">
-                Login
-              </Link>
-            </p>
-            <button className={st.button} type="submit">
-              Registre-se
-            </button>
-          </Form>
+              <p className={st.register}>
+                Já tem cadastro?{" "}
+                <Link className={st.link} to="/login">
+                  Login
+                </Link>
+              </p>
+              <button
+                className={st.button}
+                type="submit"
+                onClick={() => setFormSubmitted(true)}
+              >
+                Registre-se
+              </button>
+            </Form>
+          )}
         </Formik>
       </div>
       <div className={st.welcome}>
