e18e6680540da48c957644bb680ec98923fd6d2e
rafael-leivas <rafaelblsteam@gmail.com>
2024-06-27 19:24:14 -0300

    Create aba itens

--- client/src/Pages/Itens/Itens.jsx
+++ client/src/Pages/Itens/Itens.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import SideBar from "../../Components/SideBar/SideBar";
 import st from "./Itens.module.css";
 import { Input, Space, Button, Modal, Flex } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 
 const { Search } = Input;
 
@@ -13,50 +14,49 @@ const Itens = () => {
     <div className={st.window}>
       <SideBar />
       <div className={st.container}>
-        <h1>Itens cadastrados</h1>
-        <div className={st.actionsItens}>
-          <Space direction="vertical">
-            <Search
-              placeholder="Pesquisar"
-              onSearch={(value) => console.log(value)}
-              enterButton
-            />
-          </Space>
-
-          <Button
-            className={st.buttonModal}
-            type="primary"
-            onClick={() => setOpen(true)}
-            
-          >
-            Adicionar Item
-          </Button>
-          <Modal
-            className={st.modal}
-            title="Modal 1000px width"
-            centered
-            open={open}
-            onOk={() => setOpen(false)}
-            onCancel={() => setOpen(false)}
-            width={1000}
-            contentBg={263238}
-          >
-            <p>some contents...</p>
-            <p>some contents...</p>
-            <p>some contents...</p>
-          </Modal>
+        <div className={st.content}>
+          <div className={st.header}>
+            <div className={st.leftHeader}>
+              <h1 className={st.title}>Itens disponíveis</h1>
+              <Search placeholder="Pesquisar" style={{ width: 250 }} />
+            </div>
+            <Button
+              className={st.buttonAdd}
+              type="primary"
+              onClick={() => setOpen(true)}
+            >
+              <PlusOutlined /> Adicionar produto
+            </Button>
+          </div>
+          <table className={st.table}>
+          <thead>
+            <tr>
+              <th>Código</th>
+              <th>Nome</th>
+              <th>Categoria</th>
+              <th>Custo</th>
+              <th>Valor</th>
+              <th>Estoque</th>
+              <th>Total</th>
+              <th>Botao</th>
+            </tr>
+          </thead>
+          
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Produto 1</td>
+              <td>Alimento</td>
+              <td>10</td>
+              <td>20</td>
+              <td>10</td>
+              <td>200</td>
+              <td>Editar</td>
+            </tr>
+          </tbody>
+        </table>
         </div>
       </div>
-
-      <Flex
-        vertical
-        gap="small"
-        style={{
-          width: "100%",
-        }}
-      >
-
-      </Flex>
     </div>
   );
 };
